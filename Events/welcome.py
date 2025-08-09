3a3eb8a8d96d1a50dc66405ee79f73eb94464ffe
kevinosswald <kevinosswald@proton.me>
2025-08-09 09:50:18 +0000

    Added a member join listener

---
+++ Events/welcome.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class welcomer(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        embed = discord.Embed(
+            title=f"Welcome, {member.name}",
+            description=f"Welcome to Development Cave! {member.mention}",
+            color=discord.Color.random()
+        )
+        url = member.avatar.url if member.avatar else str(member.default_avatar)
+        embed.set_thumbnail(url=url)
+        embed.set_footer(text=f"ID: {member.id}")
+        
+        welcome_ch = discord.utils.get(member.guild.channels, id=1392616607456563353)
+        await welcome_ch.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(welcomer(bot))
+    print("Welcomer event cog loaded.")
