125c161e8aac1c3ea91c78095b8acb45c5bf02cb
kevinosswald <kevinosswald@proton.me>
2025-08-09 16:46:17 +0000

    Added a messagelogger.

---
+++ Events/messagelogger.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from typing import Optional
+
+def create_embed(user: discord.Member, action, before: discord.Message, after: Optional[discord.Message] = None):
+    if action == "deleted":
+        embed = discord.Embed(
+            description=f"{user.mention} has deleted a message!\n\n`{before.content}`"
+        )
+    elif action == "edited":
+        embed = discord.Embed(
+            description=f"{user.mention} has edited a message!\n\n`{before.content}`\nto\n`{after.content}`"
+        )
+    return embed
+
+class messagelogger(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: discord.Message):
+        chat_logs = discord.utils.get(message.guild.channels, id=1403775143351943208)
+        embed = create_embed(message.author, "deleted", message)
+        await chat_logs.send(embed=embed)
+    
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        chat_logs = discord.utils.get(before.guild.channels, id=1403775143351943208)
+        embed = create_embed(before.author, "edited", before, after)
+        await chat_logs.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(messagelogger(bot))
+    print("Message Logger event cog loaded.")
